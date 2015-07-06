dabd01373ba3f045da53cc8c786e9d5b5a133e8e
Dave Beshero <dbeshero@gmail.com>
2015-07-06 10:44:40 -0400

    mobile styles

--- js/main.js
+++ js/main.js
@@ -16,3 +16,37 @@ setInterval(function() {
 }, 1000);
 
 
+function hasClass(ele, cls) {
+  return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+function addClass(ele, cls) {
+  if (!hasClass(ele,cls)) ele.className += ' ' + cls;
+}
+
+function removeClass(ele, cls) {
+  if (hasClass(ele, cls)) {
+    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+    ele.className=ele.className.replace(reg,' ');
+  }
+}
+
+function toggleClass(ele, cls) {
+  if (hasClass(ele, cls)) {
+    removeClass(ele, cls);
+  } else {
+    addClass(ele, cls);
+  }
+}
+
+var mobileNav = document.getElementById('mobile-nav-btn');
+var container = document.getElementById('container');
+var body      = document.getElementsByTagName("BODY")[0];
+
+mobileNav.addEventListener('click', function() { 
+  toggleClass(body, 'open');
+});
+
+container.addEventListener('click', function() {
+  removeClass(body, 'open');  
+});
